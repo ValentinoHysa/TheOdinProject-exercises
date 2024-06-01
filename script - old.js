49e7c0f005bf65d6e8b82c2aa7129c08d8503a8c
ValentinoHysaMistral <valentino.hysa@mistralsolutions.it>
2024-06-01 19:08:51 +0200

    first commit

---
+++ script - old.js
@@ -0,0 +1,44 @@
+
+function playGame(){
+    let scores = { player: 0, computer: 0 };
+
+    while (scores.player < 5 && scores.computer < 5) {
+    let playerSelection = prompt("Make your choice: ROCK, PAPER, SCISSORS");
+    playRound(playerSelection, scores);
+    }
+    alert("GAME OVER");
+}
+
+function playRound(playerSelection, scores){
+    let computerSelection = randomizeChoice();
+    if (playerSelection === computerSelection){
+        alert("TIE, your score: " + scores.player + " computer score: " + scores.computer);
+    }
+    if ((playerSelection == "ROCK" && computerSelection == 'SCISSORS')
+    ||  (playerSelection == "SCISSORS" && computerSelection == 'PAPER')
+    ||  (playerSelection == "PAPER" && computerSelection == 'ROCK')){
+            scores.player++;
+            alert("YOU WIN, your score: " + scores.player + " computer score: " + scores.computer);
+    }
+    if ((playerSelection == "ROCK" && computerSelection == 'PAPER')
+        ||  (playerSelection == "SCISSORS" && computerSelection == 'ROCK')
+        ||  (playerSelection == "PAPER" && computerSelection == 'SCISSORS')){
+                scores.computer++;
+                alert("YOU LOSE, your score: " + scores.player + " computer score: " + scores.computer);
+        }
+}
+
+function randomizeChoice(){
+    let choice = Math.floor(Math.random() * 3)
+    switch (choice){
+    case 0:
+        return 'ROCK';
+    case 1:
+        return 'PAPER';
+    case 2:
+        return 'SCISSORS';
+    }
+}
+
+playGame();
+//Studiati getElementById, addEventListener, handleClick
