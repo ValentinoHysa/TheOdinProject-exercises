ff089af072e5e09576c5d9897b2f6ec72ff721e1
ValentinoHysaMistral <valentino.hysa@mistralsolutions.it>
2024-07-31 14:18:58 +0200

    Add some UI, need to fix .result

--- rps/script.js
+++ rps/script.js
@@ -11,7 +11,22 @@ function playGame(){
 let scores = { player: 0, computer: 0 };
 const buttons = document.querySelectorAll('.game-btn');
 
-buttons.forEach(button => {
+const linkButton = document.querySelector(".buttons");
+
+function hover(element, enter, leave){
+    element.addEventListener('mouseenter', enter)
+    element.addEventListener('mouseleave', leave)
+  }
+
+  buttons.forEach(button => {
+    hover(button, e => {
+        // On hover
+        e.target.setAttribute("style", "background-color: #bd93f9;");
+    }, e => {
+        // On exit hover
+        e.target.setAttribute("style", "background-color: #3882f6;");
+    });
+
     button.addEventListener('click', () => {
         const playerSelection = button.id.toUpperCase();
         playRound(playerSelection, scores);
@@ -20,6 +35,11 @@ buttons.forEach(button => {
 
 function playRound(playerSelection, scores){
     let computerSelection = randomizeChoice();
+    const myDiv = document.querySelector(".result");
+    const content = document.createTextNode("The computer picked " + computerSelection); //TODO: How to make it go to a new line
+    // content.setAttribute = ("style", "color: #white;"); //TODO: fix
+    myDiv.appendChild(content);
+    
     if (playerSelection === computerSelection){
         alert("TIE, your score: " + scores.player + " computer score: " + scores.computer);
     }
